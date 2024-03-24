a5f75bfd37766c90443069f0f021426f0c9761bc
lscolamiero <lautaro.scolamiero@gmail.com>
2024-03-24 13:51:57 -0700

    index fixed for views

--- petfax/pet.py
+++ petfax/pet.py
@@ -8,3 +8,7 @@ bp = Blueprint('pet', __name__, url_prefix='/pets')
 @bp.route('/')
 def index():
 	return render_template('index.html', pets=pets)
+
+@bp.route('/<int:id>')
+def show_pet(id):
+	return render_template('show_pet.html', pet=next(filter(lambda p: p['pet_id'] == id, pets)))
